137758b73dc74bd9bdb3940445a3334cb94ac35a
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2023-09-24 15:48:58 -0700

    Initial tokenizer

--- src/tokenize.ts
+++ src/tokenize.ts
@@ -1,3 +1,5 @@
+import TM from './tokens';
+
 type Token = {
   type: string;
   value: string;
@@ -7,10 +9,20 @@ export const tokenize = (input: string): Token[] => {
   if (!input) {
     throw new Error('Input string required.');
   }
-  console.log({ input });
   const raw = input.split(/\W+/);
-  console.log({ raw });
   const tokens: Token[] = [];
+  raw.forEach(r => {
+    const keys = Object.keys(TM);
+    for (let i = 0; i < keys.length; i += 1) {
+      const k = keys[i];
+      // @ts-expect-error Not sure why
+      const re = new RegExp(TM[k].test);
+      if (re.test(r)) {
+        tokens.push({ type: k, value: r });
+        break;
+      }
+    }
+  });
   return tokens;
 };
 
--- src/tokens.ts
+++ src/tokens.ts
@@ -1,9 +1,15 @@
+export type TokenDef = {
+  test: string;
+};
+
+export type TokenMap = Record<string, TokenDef>;
+
 export const tokens = {
   minutes: {
     test: '^(minutes|minute|mins|min)$',
   },
   unknown: {
-    test: `*`,
+    test: ``,
   },
 };
 
