9e5190d05b2df9ebb2d26f4a6b8b0f7906c20463
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2023-09-23 18:17:26 -0700

    Configure tests

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+const config = {
+  transform: {
+    '^.+\\.(t|j)sx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
+
+module.exports = config;
