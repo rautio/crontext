1c48a070141143607cc77732fb03d8a4b7fb1db3
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2023-10-02 20:57:44 -0700

    Update github link

--- demo/src/components/Header.tsx
+++ demo/src/components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import ExternalLink from '@/icons/ExternalLink';
 import Link from 'next/link';
+import Github from '@/icons/Github';
 
 export const Header = () => {
   return (
@@ -18,7 +19,7 @@ export const Header = () => {
         <div className="flex leading-[60px] hover:text-sky-500">
           <span>Github</span>
           <span className="m-auto pl-2">
-            <ExternalLink />
+            <Github />
           </span>
         </div>
       </Link>
