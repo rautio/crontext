771bff841fda5a2fc9de5c5260a1e3f848c1812e
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2023-10-02 20:52:43 -0700

    Implement copy

--- demo/src/components/Editor.tsx
+++ demo/src/components/Editor.tsx
@@ -1,11 +1,18 @@
 'use client';
-import React, { useState } from 'react';
-import { Input } from '@/components/ui/input';
+import React, { useState, useEffect } from 'react';
 import parseCron from 'crontext';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import Copy from '@/icons/Copy';
 
 export const Editor = () => {
   const [text, setText] = useState('Every minute');
-  console.log(parseCron(text));
+  const [isCopied, setIsCopied] = useState(false);
+  useEffect(() => {
+    if (isCopied) {
+      setTimeout(() => setIsCopied(false), 2000);
+    }
+  }, [isCopied]);
   return (
     <div className="w-full flex flex-col">
       <Input
@@ -13,9 +20,28 @@ export const Editor = () => {
         onChange={e => setText(e.target.value)}
         className="text-xl p-8 dark:bg-neutral-900 bg-white border-sky-500"
       />
-      <span className="text-3xl tracking-[0.75rem] pt-12 m-auto">
-        {parseCron(text)}
-      </span>
+      <div className="flex pt-12 m-auto">
+        <Input
+          readOnly
+          value={parseCron(text)}
+          className="text-2xl tracking-[0.75rem] w-30 text-center"
+        />
+        <Button
+          onClick={() => {
+            setIsCopied(true);
+            navigator.clipboard.writeText(text);
+          }}
+        >
+          {isCopied ? (
+            <span className="text-sky-500">Copied!</span>
+          ) : (
+            <span className="flex leading-4">
+              <Copy />
+              Copy
+            </span>
+          )}
+        </Button>
+      </div>
     </div>
   );
 };
