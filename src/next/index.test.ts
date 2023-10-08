41cd157d49a56c7072751c48e298c7353fcb524f
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2023-10-07 20:26:39 -0700

    Implement next occurence

--- src/next/index.test.ts
+++ src/next/index.test.ts
@@ -1,10 +1,224 @@
-import { nextDate } from '.';
+import {
+  nextDate,
+  iterMonth,
+  iterDayOfWeek,
+  iterDayOfMonth,
+  iterHour,
+  iterMinute,
+  getNextValue,
+  getDiff,
+  getNums,
+  isValid,
+} from '.';
 
 describe('nextDate() should', () => {
-  test('return the correct next minute', () => {
-    const n = nextDate('* * * * *', new Date('Wed October 4 09:00:00 2023'));
+  test('return the correct next month', () => {
+    const n = nextDate('* * * 10-11 *', new Date('October 4 09:00:00 2023'));
     expect(n.toString()).toEqual(
-      new Date('Wed October 4 9:01:00 2023').toString(),
+      new Date('November 4 9:00:00 2023').toString(),
     );
+    expect(
+      nextDate('* * 13 * *', new Date('April 19 13:00 2023')).toString(),
+    ).toEqual(new Date('May 13 13:00 2023').toString()); // This should actually be 0:0
+  });
+  test('return the correct next week', () => {
+    const n = nextDate('* * * * 0,6', new Date('March 28 11:00:00 2023'));
+    expect(n.toString()).toEqual(new Date('April 1 11:00:00 2023').toString());
+  });
+  test('return exact datetime', () => {
+    expect(
+      nextDate('25 8 31 9 *', new Date('March 28 11:00:00 2023')).toString(),
+    ).toEqual(new Date('October 31 8:25:00 2023').toString());
+  });
+});
+
+describe('getNums() should', () => {
+  test('Return empty array on *', () => {
+    expect(getNums('*', 60)).toEqual([]);
+  });
+  test('Return a single number', () => {
+    expect(getNums('2', 24)).toEqual([2]);
+    expect(getNums('0', 24)).toEqual([0]);
+  });
+  test('Return numbers given comma separated list', () => {
+    expect(getNums('2,3,4', 24)).toEqual([2, 3, 4]);
+    expect(getNums('0,1', 24)).toEqual([0, 1]);
+    expect(getNums('10, 24', 60)).toEqual([10, 24]);
+  });
+  test('Return numbers given a range', () => {
+    expect(getNums('2-4', 24)).toEqual([2, 3, 4]);
+    expect(getNums('22-3', 24)).toEqual([0, 1, 2, 3, 22, 23]);
+  });
+  test('Return combo of range and comma separated', () => {
+    expect(getNums('2-4,12', 24)).toEqual([2, 3, 4, 12]);
+    expect(getNums('22-3, 17', 24)).toEqual([0, 1, 2, 3, 17, 22, 23]);
+    expect(getNums('22-3, 4-8', 24)).toEqual([
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 22, 23,
+    ]);
+  });
+});
+
+describe('iterMonth() should', () => {
+  test('return the same date on a valid month', () => {
+    const c = new Date('October 4 9:00 2023');
+    const res = iterMonth(c, [1, 2, 9, 11, 12]);
+    expect(res.getMonth()).toEqual(9);
+    expect(res).toEqual(c);
+  });
+  test('iterate to a month next year', () => {
+    const c1 = new Date('October 4 9:00 2023');
+    const res1 = iterMonth(c1, [2, 4, 8]);
+    expect(res1.getMonth()).toEqual(2);
+    expect(res1.toString()).toEqual(new Date('March 4 9:00 2024').toString());
+    const c2 = new Date('February 4 9:00 2023');
+    const res2 = iterMonth(c2, [0]);
+    expect(res2.getMonth()).toEqual(0);
+    expect(res2.toString()).toEqual(new Date('January 4 9:00 2024').toString());
+  });
+  test('iterate to the next month', () => {
+    const c1 = new Date('October 4 9:00 2023');
+    const res1 = iterMonth(c1, [2, 4, 10]);
+    expect(res1.getMonth()).toEqual(10);
+    expect(res1.toString()).toEqual(
+      new Date('November 4 9:00 2023').toString(),
+    );
+    const c2 = new Date('February 4 9:00 2023');
+    const res2 = iterMonth(c2, [3]);
+    expect(res2.getMonth()).toEqual(3);
+    expect(res2.toString()).toEqual(new Date('April 4 9:00 2023').toString());
+  });
+});
+
+describe('iterDayOfWeek() should', () => {
+  test('return the same date on a valid day of week', () => {
+    const c = new Date('April 16, 13:00 2023');
+    const res = iterDayOfWeek(c, [0, 1, 2, 3, 4, 5, 6]);
+    expect(res.getDay()).toEqual(0);
+    expect(res.toString()).toEqual(new Date('April 16, 13:00 2023').toString());
+  });
+  test('iterate to the next month', () => {
+    const c = new Date('April 29, 13:00 2023');
+    const res = iterDayOfWeek(c, [1, 2, 3, 4, 5]);
+    expect(res.getDay()).toEqual(1);
+    expect(res.toString()).toEqual(new Date('May 1, 13:00 2023').toString());
+  });
+  test('iterate to the next week', () => {
+    const c = new Date('April 17, 13:00 2023');
+    const res = iterDayOfWeek(c, [0, 6]);
+    expect(res.getDay()).toEqual(6);
+    expect(res.toString()).toEqual(new Date('April 22, 13:00 2023').toString());
+  });
+});
+
+describe('iterDayOfMonth() should', () => {
+  test('return the same date on a valid day of month', () => {
+    const c = new Date('April 16, 13:00 2023');
+    const res = iterDayOfMonth(c, [16]);
+    expect(res.toString()).toEqual(new Date('April 16, 13:00 2023').toString());
+  });
+  test('iterate to the next month', () => {
+    const c = new Date('April 29, 13:00 2023');
+    const res = iterDayOfMonth(c, [15, 22, 27]);
+    expect(res.getDate()).toEqual(15);
+    expect(res.toString()).toEqual(new Date('May 15, 13:00 2023').toString());
+  });
+  test('iterate to the next week', () => {
+    const c = new Date('April 17, 13:00 2023');
+    const res = iterDayOfMonth(c, [14, 19, 22, 5]);
+    expect(res.getDate()).toEqual(19);
+    expect(res.toString()).toEqual(new Date('April 19, 13:00 2023').toString());
+  });
+});
+
+describe('iterHour() should', () => {
+  test('return the same date on a valid hour', () => {
+    const c = new Date('April 16, 13:00 2023');
+    const res = iterHour(c, [13]);
+    expect(res.toString()).toEqual(new Date('April 16, 13:00 2023').toString());
+  });
+  test('iterate to the next day', () => {
+    const c = new Date('September 13, 13:00 2023');
+    const res = iterHour(c, [9, 10, 4]);
+    expect(res.getHours()).toEqual(4);
+    expect(res.toString()).toEqual(
+      new Date('September 14, 04:00 2023').toString(),
+    );
+  });
+  test('iterate to the next hour', () => {
+    const c = new Date('April 17, 13:00 2023');
+    const res = iterHour(c, [19, 22, 5]);
+    expect(res.getHours()).toEqual(19);
+    expect(res.toString()).toEqual(new Date('April 17, 19:00 2023').toString());
+  });
+});
+
+describe('iterMinute() should', () => {
+  test('iterate to the next hour', () => {
+    const c = new Date('September 13, 13:45 2023');
+    const res = iterMinute(c, [9, 10, 4]);
+    expect(res.getMinutes()).toEqual(4);
+    expect(res.toString()).toEqual(
+      new Date('September 13, 14:04 2023').toString(),
+    );
+  });
+  test('iterate to the next mninute', () => {
+    const c = new Date('April 17, 13:00 2023');
+    const res = iterMinute(c, [19, 22, 5]);
+    expect(res.getMinutes()).toEqual(5);
+    expect(res.toString()).toEqual(new Date('April 17, 13:05 2023').toString());
+  });
+});
+
+describe('getNextValue() should', () => {
+  test('return the current value if valid', () => {
+    const next = getNextValue(10, [1, 2, 3, 19, 10]);
+    expect(next).toEqual(10);
+  });
+  test('wrap to the beginning of the schedule', () => {
+    const next = getNextValue(10, [2, 3, 8, 9]);
+    expect(next).toEqual(2);
+  });
+  test('return the next value in order', () => {
+    const next = getNextValue(4, [1, 2, 6, 7]);
+    expect(next).toEqual(6);
+  });
+  test('return current if no schedule', () => {
+    const next = getNextValue(12, []);
+    expect(next).toEqual(12);
+  });
+});
+
+describe('getDiff() should', () => {
+  test('return a valid diff when the next number is in sequence', () => {
+    expect(getDiff(5, 10, 12)).toEqual(5);
+    expect(getDiff(2, 6, 7)).toEqual(4);
+    expect(getDiff(25, 50, 60)).toEqual(25);
+    expect(getDiff(0, 12, 24)).toEqual(12);
+  });
+  test('return a valid diff skipping the max', () => {
+    // month
+    expect(getDiff(4, 2, 12)).toEqual(10);
+    expect(getDiff(11, 6, 12)).toEqual(7);
+    // day of week
+    expect(getDiff(4, 1, 7)).toEqual(4);
+    expect(getDiff(2, 1, 7)).toEqual(6);
+    expect(getDiff(6, 0, 7)).toEqual(1);
+    // hour
+    expect(getDiff(18, 2, 24)).toEqual(8);
+    expect(getDiff(23, 2, 24)).toEqual(3);
+    expect(getDiff(0, 2, 24)).toEqual(2);
+    // minute
+    expect(getDiff(55, 5, 60)).toEqual(10);
+    expect(getDiff(40, 23, 60)).toEqual(43);
+    expect(getDiff(10, 23, 60)).toEqual(13);
+  });
+});
+
+describe('isValid() should', () => {
+  test('return correct response', () => {
+    expect(isValid(4, [1, 2, 3])).toEqual(false);
+    expect(isValid(4, [])).toEqual(true);
+    expect(isValid(4, [4])).toEqual(true);
+    expect(isValid(4, [4, 1, 12])).toEqual(true);
   });
 });
--- src/next/index.ts
+++ src/next/index.ts
@@ -4,7 +4,7 @@
  * @param str
  * @returns
  */
-export const getNums = (str: string): Array<number> => {
+export const getNums = (str: string, units: number): Array<number> => {
   const isNum = !isNaN(Number(str));
   let res: Array<number> = [];
   if (isNum) {
@@ -12,7 +12,7 @@ export const getNums = (str: string): Array<number> => {
   } else if (str.indexOf(',') > -1) {
     const splits = str.split(',');
     splits.forEach(split => {
-      res = [...res, ...getNums(split)];
+      res = [...res, ...getNums(split, units)];
     });
   } else if (str.indexOf('-') > -1) {
     // There's a range - but we've already split so this should be just two numbers
@@ -20,14 +20,14 @@ export const getNums = (str: string): Array<number> => {
     if (splits.length === 2) {
       const start = Number(splits[0]);
       const end = Number(splits[1]);
-      if (!isNaN(start) && !isNaN(end)) {
-        for (let i = start; i++; i <= end) {
-          res.push(i);
-        }
+      const diff = getDiff(start, end, units);
+      for (let i = 0; i <= diff; i++) {
+        res.push((start + i) % units);
       }
     }
   }
-  return res.sort();
+  // '*' returns empty array.
+  return res.sort((a, b) => a - b);
 };
 /**
  * Given an array of possible next values find the closest one that comes next in the order.
@@ -36,20 +36,170 @@ export const getNums = (str: string): Array<number> => {
  * @returns
  */
 export const getNextValue = (cur: number, nums: Array<number>): number => {
-  for (let i = 0; i < nums.length; i++) {
-    const num = nums[i];
+  // Nums has to always have at least 1 value
+  // Otherwise all values are valid and we don't call this method
+  // But for safety we just return the current number;
+  if (nums.length === 0) return cur;
+  const sorted = nums.sort((a, b) => a - b);
+  for (let i = 0; i < sorted.length; i++) {
+    const num = sorted[i];
     if (num >= cur) {
       return num;
     }
   }
-  return -1;
+  // If we didn't find a number, it means our cur is the largest so we need to
+  // wrap and return the next smallest item in the schedule.
+  return nums[0];
 };
 
-export const getDiffMinutes = (cur: number, next: number) => {
+/**
+ * Whether the given number is part of a schedule.
+ * @param num
+ * @param nums
+ * @returns
+ */
+export const isValid = (num: number, nums: Array<number>) => {
+  // If nums is empty - all values are valid
+  if (nums.length === 0) return true;
+  return nums.indexOf(num) > -1;
+};
+
+/**
+ * Get the difference between two time numbers. If the first is less than the second
+ * we go around the clock to get to it. Not backwards in time. No time traveling!
+ * @param cur
+ * @param next
+ * @param max
+ * @returns
+ */
+export const getDiff = (cur: number, next: number, units: number) => {
   if (next < cur) {
-    return 60 - (cur - next);
+    return units - cur + next;
   }
   return next - cur;
+};
+
+/**
+ * Iterate to the next month according to the given schedule
+ * @param cur
+ * @param cronSplit
+ */
+export const iterMonth = (cur: Date, schedule: number[]) => {
+  if (!isValid(cur.getMonth(), schedule)) {
+    // Current month is not valid - iterate.
+    const next = getNextValue(cur.getMonth(), schedule);
+    // Calculate diff in case the month is behind us - then it'll bump the year
+    const diff = getDiff(cur.getMonth(), next, 12);
+    cur.setMonth(cur.getMonth() + diff);
+    // There is no year format so no need to check if we had spill over
+  }
+  return cur;
+};
+
+/**
+ * Iterate to the next day of week given the schedule.
+ * @param cur
+ * @param schedule
+ * @returns
+ */
+export const iterDayOfWeek = (cur: Date, schedule: number[]): Date => {
+  if (!isValid(cur.getDay(), schedule)) {
+    // Current day of week is not valid - iterate
+    const next = getNextValue(cur.getDay(), schedule);
+    const diff = getDiff(cur.getDay(), next, 7);
+    cur.setDate(cur.getDate() + diff);
+  }
+  return cur;
+};
+
+/**
+ * Iterate to the next day of month.
+ * @param cur
+ * @param schedule
+ * @returns
+ */
+export const iterDayOfMonth = (cur: Date, schedule: number[]): Date => {
+  if (!isValid(cur.getDate(), schedule)) {
+    const next = getNextValue(cur.getDate(), schedule);
+    // Don't know how many days are in each month so just bump by 1 if the month is behind us
+    if (cur.getDate() > next) {
+      cur.setMonth(cur.getMonth() + 1);
+    }
+    cur.setDate(next);
+  }
+  return cur;
+};
+
+/**
+ * Iterate to the next hour.
+ * @param cur
+ * @param schedule
+ */
+export const iterHour = (cur: Date, schedule: number[]): Date => {
+  if (!isValid(cur.getHours(), schedule)) {
+    const curHours = cur.getHours();
+    const next = getNextValue(curHours, schedule);
+    const diff = getDiff(curHours, next, 24);
+    cur.setHours(curHours + diff);
+  }
+  return cur;
+};
+
+export const iterMinute = (cur: Date, schedule: number[]): Date => {
+  if (!isValid(cur.getMinutes(), schedule)) {
+    const curMinutes = cur.getMinutes();
+    const next = getNextValue(curMinutes, schedule);
+    const diff = getDiff(curMinutes, next, 60);
+    cur.setMinutes(curMinutes + diff);
+  }
+  return cur;
+};
+
+/**
+ * Iterate to the next date on the cron schedule. Does not support seconds format.
+ * @param date
+ * @param cron
+ * @returns
+ */
+export const iterDate = (date: Date, cron: string): Date => {
+  // Keep dates idompotent since this is called recursively.
+  let cur = new Date(date.getTime());
+  const splits = cron.split(' ');
+  const [minute, hour, dayMonth, month, dayWeek] = splits;
+  // Validate largest items first (smaller ones will spill over and re-run checks)
+  const monthSchedule = getNums(month, 11);
+  const dayWeekSchedule = getNums(dayWeek, 7);
+  const dayMonthSchedule = getNums(dayMonth, 31); // How do we know? Depends on the month.
+  const hourSchedule = getNums(hour, 24);
+  const minuteSchedule = getNums(minute, 60);
+  // Validate minute
+  const curMinutes = cur.getMinutes();
+  cur = iterMinute(cur, minuteSchedule);
+  if (curMinutes > cur.getMinutes()) {
+    return iterDate(cur, cron);
+  }
+  // Validate hour
+  const curHours = cur.getHours();
+  cur = iterHour(cur, hourSchedule);
+  if (curHours > cur.getHours()) {
+    // We have spilled over the day - need to revalidate
+    return iterDate(cur, cron);
+  }
+  // Validate date
+  cur = iterDayOfMonth(cur, dayMonthSchedule);
+  // Validate day of week
+  cur = iterDayOfWeek(cur, getNums(dayWeek, 7));
+  // Validate month
+  cur = iterMonth(cur, getNums(month, 12));
+  if (
+    !isValid(cur.getMonth(), monthSchedule) ||
+    !isValid(cur.getDay(), dayWeekSchedule) ||
+    !isValid(cur.getDate(), dayMonthSchedule)
+  ) {
+    // We broke the month or day schedule, need to reiterate
+    return iterDate(cur, cron);
+  }
+  return cur;
 };
 
 /**
@@ -57,54 +207,11 @@ export const getDiffMinutes = (cur: number, next: number) => {
  * @param cron
  */
 export const nextDate = (cron: string, startDate?: Date): Date => {
-  const cur = startDate ? new Date(startDate.getTime()) : new Date(); // Either given start date or the current date
   const splits = cron.split(' ');
-  const next = startDate ? new Date(startDate.getTime()) : new Date();
+  const start = startDate || new Date();
+  // If length is 6 then there are seconds.
   if (splits.length === 5) {
-    // 6 would denote we're using seconds
-    splits.forEach((item, i) => {
-      // Formats:
-      // num
-      // num-num
-      // num,num,num
-      // num/num
-      switch (i) {
-        case 0: {
-          let add = 0;
-          // minutes
-          if (item === '*') {
-            add = 1;
-          } else if (!isNaN(Number(item))) {
-            next.setMinutes(Number(item));
-            add = getDiffMinutes(cur.getMinutes(), Number(item));
-          } else {
-            const c = cur.getMinutes();
-            const nums = getNums(item);
-            add = getDiffMinutes(cur.getMinutes(), getNextValue(c, nums));
-          }
-          next.setMinutes(cur.getMinutes() + add);
-          break;
-        }
-        case 1: {
-          // hour
-          break;
-        }
-        case 2: {
-          // day of month
-          break;
-        }
-        case 3: {
-          // month
-          break;
-        }
-        case 4: {
-          // day of week
-          break;
-        }
-        default:
-          break;
-      }
-    });
+    return iterDate(start, cron);
   }
-  return next;
+  return start;
 };
