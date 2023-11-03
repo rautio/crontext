0bfa72e4738edb0fa45e286c8d8ff8da1b267d1b
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2023-11-03 10:29:38 -0700

    Use dark theme

--- demo/src/app-editor.ts
+++ demo/src/app-editor.ts
@@ -1,6 +1,6 @@
 import { LitElement, html, unsafeCSS } from 'lit';
 import { customElement } from 'lit/decorators.js';
-import styles from './app-editor.css';
+import styles from './app-editor.css?inline';
 
 @customElement('app-editor')
 export class App extends LitElement {
